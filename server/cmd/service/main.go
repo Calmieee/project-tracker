fe52dcd3bf1729ef5f4b2f0c630161ecb32c8784
Calmie <safiullinrafa@yandex.ru>
2025-04-14 06:23:13 +0300

    fix: cors

--- server/cmd/service/main.go
+++ server/cmd/service/main.go
@@ -53,11 +53,11 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
+		AllowOrigins:     []string{"https://project-tracker.calmieee-projects.ru"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: false,
+		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
 
