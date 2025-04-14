2aa8e354ac98cb0c67c959dcec17aa5e55a015d4
Calmie <safiullinrafa@yandex.ru>
2025-04-14 06:18:19 +0300

    feat: add allowHost

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['project-tracker.calmieee-projects.ru'],
+  },
 });
